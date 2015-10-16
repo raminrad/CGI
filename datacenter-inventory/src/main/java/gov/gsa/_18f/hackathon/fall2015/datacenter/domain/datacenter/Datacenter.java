342b02a644a7e982e79e16120fd80632a6cec8e5
dfladung <david.fladung@gmail.om>
2015-10-16 10:46:15 -0400

    latest

--- datacenter-inventory/src/main/java/gov/gsa/_18f/hackathon/fall2015/datacenter/domain/datacenter/Datacenter.java
+++ datacenter-inventory/src/main/java/gov/gsa/_18f/hackathon/fall2015/datacenter/domain/datacenter/Datacenter.java
@@ -1,12 +1,21 @@
 package gov.gsa._18f.hackathon.fall2015.datacenter.domain.datacenter;
 
+import java.util.List;
+
 import gov.gsa._18f.hackathon.fall2015.datacenter.domain.common.BaseEntity;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
+import javax.persistence.CascadeType;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 /**
  * @author dfladung
@@ -28,5 +37,54 @@ public class Datacenter extends BaseEntity {
 
 	@Embedded
 	Address address;
+
+	@OneToMany(cascade = { CascadeType.ALL }, fetch = FetchType.LAZY, orphanRemoval = true)
+	@Fetch(value = FetchMode.SUBSELECT)
+	@JoinColumn(name = "datacenter_id", nullable = false)
+	List<QuarterlyData> quarterlyData;
+
+	public Datacenter() {
+		datacenterInformation = new DatacenterInformation();
+		providerInformation = new ProviderInformation();
+		address = new Address();
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Datacenter [datacenterInformation=%s, providerInformation=%s, address=%s]",
+				datacenterInformation, providerInformation, address);
+	}
+
+	public DatacenterInformation getDatacenterInformation() {
+		return datacenterInformation;
+	}
+
+	public void setDatacenterInformation(DatacenterInformation datacenterInformation) {
+		this.datacenterInformation = datacenterInformation;
+	}
+
+	public ProviderInformation getProviderInformation() {
+		return providerInformation;
+	}
+
+	public void setProviderInformation(ProviderInformation providerInformation) {
+		this.providerInformation = providerInformation;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public List<QuarterlyData> getQuarterlyData() {
+		return quarterlyData;
+	}
+
+	public void setQuarterlyData(List<QuarterlyData> quarterlyData) {
+		this.quarterlyData = quarterlyData;
+	}
 
 }
--- datacenter-inventory/src/main/java/gov/gsa/_18f/hackathon/fall2015/datacenter/domain/datacenter/DatacenterInformation.java
+++ datacenter-inventory/src/main/java/gov/gsa/_18f/hackathon/fall2015/datacenter/domain/datacenter/DatacenterInformation.java
@@ -1,5 +1,6 @@
 package gov.gsa._18f.hackathon.fall2015.datacenter.domain.datacenter;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 import gov.gsa._18f.hackathon.fall2015.datacenter.domain.common.BaseEntity;
@@ -12,5 +13,11 @@ import gov.gsa._18f.hackathon.fall2015.datacenter.domain.common.BaseEntity;
 public class DatacenterInformation extends BaseEntity {
 
 	private static final long serialVersionUID = 1L;
+
+	@Column(name = "datacenter_id", unique = true, nullable = false)
+	String datacenterId;
+
+	@Column(name = "ageny_datacenter_id", nullable = false)
+	String agencyDataCenterId;
 
 }
